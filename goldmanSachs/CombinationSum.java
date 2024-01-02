3f7da8f58bcbacc25c66376ad75c05329ec4340d
vikram <102456477+Vkperfect@users.noreply.github.com>
2024-01-02 22:18:03 +0530

    Create CombinationSum.java

---
+++ goldmanSachs/CombinationSum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result=new ArrayList<>();
+        backtrack(result, new ArrayList<>(), k, n, 1);
+        return result;
+    }
+
+    private static void backtrack(List<List<Integer>> result, List<Integer> current, int k, int remaining, int start) {
+        if (k == 0 && remaining == 0) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            if (i > remaining) {
+                break;
+            }
+
+            current.add(i);
+            backtrack(result, current, k - 1, remaining - i, i + 1);
+            current.remove(current.size() - 1);
+        }
+    }
+    
+}
